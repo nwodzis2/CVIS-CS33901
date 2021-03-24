b9d9e055f96eb85da95be3216684a343afdea25c
nwodzis2 <60149019+nwodzis2@users.noreply.github.com>
2021-03-23 23:46:20 -0400

    made base cpp and h files

---
+++ Week_3_stuff/Classes_headers/vaccinated.h
@@ -0,0 +1,17 @@
+#ifndef appointment_h
+#define appointment_h
+
+/****************************************************************
+ * Class definition : Appointment
+ * @author Jaden Kandel, Steven Strange, Nathan Woodzisz
+ * **************************************************************/
+
+class Appointment {
+
+public:
+
+private:
+
+};
+
+#endif
