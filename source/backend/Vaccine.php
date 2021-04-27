662a0cd3b7af10f159182827f228a040590faa0a
sstrange259 <sstrange@kent.edu>
2021-04-27 00:39:32 -0400

    Vaccine class

---
+++ source/backend/Vaccine.php
@@ -0,0 +1,33 @@
+<?php
+    include_once("../backend/db.php");
+
+    $DB_link = new DB_Link();
+    $connection = $DB_link->connect("localhost", "cvis");
+
+    class Vaccine{
+
+     private $brand_name;
+
+     //setter
+     function set_vaccine_name(){
+        $this->$brand_name = $brand_name;
+      }
+
+     //getters
+     function get_vaccine_name(){
+        return $this->$brand_name;
+      }
+      // returns 1 for johnson and johnson and 2 for pfizer or moderna 
+     function get_vaccine_doses(){
+         if ($brand_name == "johnson and johnson")
+         {
+             return 1;
+         } 
+         else if ($brand_name == "pfizer" || $brand_name == "moderna") {
+             return 2;
+         }
+         else{
+            echo "invalid vaccine name";
+         }
+     }
+ }
